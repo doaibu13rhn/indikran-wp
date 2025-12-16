250af3a1eb73309f6bafabcaec52dde116dd6083
doaibu13rhn <alkaffraihan13@gmail.com>
2025-12-16 21:33:28 +0700

    update: add assets image, installing flowbite for framework, and trial error for navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.php",
     "./**/*.php",
+    "./src/**/*.{js,css}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
