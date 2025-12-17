e7388f73b26f6b1bcfa0f6a0f5ac1cca430b1723
doaibu13rhn <alkaffraihan13@gmail.com>
2025-12-17 22:06:38 +0700

    update: copying assets image from react repo and make some updates for home page

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 
     <nav class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/indikran-logo.webp" alt="Indikran Logo"
             class="h-8" />
 
--- home.php
+++ home.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 /* Template Name: Home */
-get_header(); 
+get_header();
 ?>
 
 <div class="container mx-auto px-4 border-8 border-red-800 mt-10">
@@ -8,7 +8,7 @@ get_header();
         <h1 class="text-4xl font-bold text-red-500 mb-4">Welcome to Indikran</h1>
         <p class="text-lg text-blue-800 mb-8">Your one-stop solution for all your needs.</p>
 
-        <a href="#services" class="bg-green-500 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-green-600 transition">
+        <a href="#services" class="bg-red-900 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-green-600 transition">
             Our Services
         </a>
     </div>
@@ -18,7 +18,7 @@ get_header();
     <h2 class="text-3xl font-bold text-center mb-8">Our Services</h2>
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        
+
         <div class="bg-gray-800 p-6 rounded-lg shadow-lg border border-gray-700">
             <h3 class="text-xl font-semibold mb-4">Service 1</h3>
             <p class="text-gray-400">Description of Service 1.</p>
